a8235ddcdded5ff869fd3eaf0c5dfebbcc838a33
Javi1004 <xavicampix@gmail.com>
2022-05-04 20:21:11 +0200

    creacion clases

--- includes/usuarios.h
+++ includes/usuarios.h
@@ -1,11 +1,49 @@
 #ifndef USUARIOS_H
 #define USUARIOS_H
+#include <QString>
+#include <iostream>
 
 
 class usuarios
 {
 public:
     usuarios();
+    QString Nombre;
+    QString Apellidos;
+    QString DNI;
+    double Edad;
+};
+
+class administrador:usuarios
+{
+public:
+    administrador();
+    double Sueldo;
+    QString dpto;
+
+};
+
+class enfermero:usuarios
+{
+public:
+    enfermero();
+    double Sueldo;
+};
+
+class tecnico:usuarios
+{
+public:
+    tecnico();
+    double Sueldo;
+    double num_laboratorio;
+};
+
+class paciente:usuarios
+{
+public:
+    paciente();
+    bool infectado;
+    double habitacion;
 };
 
 #endif // USUARIOS_H
