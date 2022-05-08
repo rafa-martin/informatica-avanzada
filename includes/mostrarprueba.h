aa1484ed4b778a0ae7643107b0630008f852676c
plrlm <pilar.lopez7@alu.uclm.es>
2022-05-08 20:54:04 +0200

    creado ventana para añadir las pruebas diagnosticas

--- includes/mostrarprueba.h
+++ includes/mostrarprueba.h
@@ -1,6 +1,6 @@
 #ifndef MOSTRARPRUEBA_H
 #define MOSTRARPRUEBA_H
-
+#include <database/database.hpp>
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +12,16 @@ class MostrarPrueba : public QDialog
     Q_OBJECT
 
 public:
-    explicit MostrarPrueba(QWidget *parent = nullptr);
+    explicit MostrarPrueba(IA_Clinic::Database &db,QWidget *parent = nullptr);
     ~MostrarPrueba();
+    void actualizar();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MostrarPrueba *ui;
+    IA_Clinic::Database &db;
 };
 
 #endif // MOSTRARPRUEBA_H
--- src/mostrarprueba.cpp
+++ src/mostrarprueba.cpp
@@ -1,14 +1,60 @@
 #include "mostrarprueba.h"
 #include "ui_mostrarprueba.h"
-
-MostrarPrueba::MostrarPrueba(QWidget *parent) :
+#include "crearprueba.h"
+#include <iostream>
+MostrarPrueba::MostrarPrueba(IA_Clinic::Database &db,QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::MostrarPrueba)
+    ui(new Ui::MostrarPrueba),
+    db(db)
 {
     ui->setupUi(this);
+    actualizar();
 }
 
 MostrarPrueba::~MostrarPrueba()
 {
     delete ui;
 }
+
+void MostrarPrueba::actualizar()
+{
+
+    vector<PruebaDiagnostica*>& db_pruebas = db.getDataVector<PruebaDiagnostica*>();
+    int cont=0;
+    ui->tableWidget->setRowCount(db_pruebas.size());
+    for (auto prueba:db_pruebas)
+    {
+        QString dni = QString::fromStdString(prueba->getdni());
+        QString nombre = QString::fromStdString(prueba->getnombre());
+        QString fecha = prueba->getFecha().toString();
+
+        //se crea una celda:
+        QTableWidgetItem* n_item = new QTableWidgetItem(dni);
+        ui->tableWidget->setItem(cont,0, n_item);
+
+        n_item = new QTableWidgetItem(nombre);
+        ui->tableWidget->setItem(cont,1, n_item);
+
+        n_item = new QTableWidgetItem(fecha);
+        ui->tableWidget->setItem(cont,2, n_item);
+
+        cont=cont+1;
+    }
+}
+
+void MostrarPrueba::on_pushButton_clicked()
+{
+    CrearPrueba temp;
+    temp.exec();
+    temp.getguardar();
+    vector<PruebaDiagnostica*>& db_pruebas = db.getDataVector<PruebaDiagnostica*>();
+
+    if (temp.getguardar()==true)
+    {
+        PruebaDiagnostica* nuevaprueba = temp.getprueba();
+        db_pruebas.push_back(nuevaprueba); //meter en base de datos pruebas
+        actualizar();
+    }
+
+}
+
