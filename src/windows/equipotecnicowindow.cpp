41187892efed7887fd6126d71ddb6a25b1415a24
TheBerbat <theverbat@gmail.com>
2022-05-09 16:53:53 +0200

    Added window title

--- src/windows/equipotecnicowindow.cpp
+++ src/windows/equipotecnicowindow.cpp
@@ -4,7 +4,10 @@
 EquipoTecnicoWindow::EquipoTecnicoWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::EquipoTecnicoWindow)
-{ ui->setupUi(this); }
+{
+    ui->setupUi(this);
+    setWindowTitle("Equipo de trabajo");
+}
 
 EquipoTecnicoWindow::~EquipoTecnicoWindow()
     { delete ui; }
